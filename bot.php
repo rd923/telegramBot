19e3eb34c7c5f698a457a96f798d48ad1d6151d1
rd923 <136409025+rd923@users.noreply.github.com>
2024-10-01 17:01:09 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,32 +1,7 @@
-<?php
+<?php 
 
+$update = file_get_contents('php://input');
 
-define('BOT_TOKEN', '8082164762:AAE6bumpUY2soEy3MthSxhae6qZHrtU2rs0');//INIT BOT TOKEN
-define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');//DEFINE APP API URL OR TELEGRAM API URL
-
-
-function sendMessage($chatID, $text)
-{
-    $url = API_URL . 'sendMessage?chat_id=' . $chatID . '&text=' . urlencode($text);
-
-    file_get_contents($url);
-}//end send message func.
-
-
-//Get The Incoming update
-$update = json_decode(file_get_contents('php://input'), true);
-
-//Check Is Update Content Message
-
-if (isset($update['message']))
-{
-    $chatID = $update['message']['chat']['id'];
-    $text = 'You Say ' . $update['message']['text'];
-
-    sendMessage($chatID, $text);
-}//end if
-
-
-    $chatID = $update['message']['chat']['id'];
-    $text = 'You Say ' . $update['message']['text'];
-sendMessage($chatID, $text);
+$file = fopen('telegram.json', 'w');
+fwrite($file, $update);
+fclose($file);
